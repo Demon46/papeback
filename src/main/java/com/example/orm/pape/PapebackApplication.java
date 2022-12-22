9869094adbbf0653c33d1166215f74bdceaff777
ozkar <oreyesmtz95@gmail.com>
2022-12-22 17:42:30 -0600

    Intento Solución CORS

--- src/main/java/com/example/orm/pape/PapebackApplication.java
+++ src/main/java/com/example/orm/pape/PapebackApplication.java
@@ -2,6 +2,9 @@ package com.example.orm.pape;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PapebackApplication {
@@ -10,4 +13,14 @@ public class PapebackApplication {
 		SpringApplication.run(PapebackApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("/**").allowedMethods("*").allowedHeaders("*");
+			}
+		};
+	}
+	
 }
